54471bb8778587a8df52c77dc53c0c3013e21aef
Andrew Turner <andrew@highearthorbit.com>
2010-03-30 01:28:38 -0700

    Fixing event code based on Adam's modifications

--- samples/js/mapstraction/mapstraction-events.js
+++ samples/js/mapstraction/mapstraction-events.js
@@ -3,17 +3,16 @@ var mapstraction;
 function initialize() {
     mapstraction = new mxn.Mapstraction('map_canvas','google');
     mapstraction.setCenterAndZoom(new mxn.LatLonPoint(37.404196,-122.008194), 7);
+    mapstraction.addLargeControls();
 
-    mapstraction.addEventListener('click', 
-    function(p) { 
+  mapstraction.click.addHandler(function(event_name, event_source, event_args) {
+    var p = event_args.location;
         marker = new mxn.Marker(p);
         text = "<p>Lat: " + p.lat + " Lon: " + p.lon + "</p>";
-        marker.setLabel(text);
         marker.setInfoBubble(text);
-        marker.openBubble();
         mapstraction.addMarker(marker);
+        marker.openBubble();
     });
-    mapstraction.addEventListener( 'changeZoom', function() { alert("changeZoom")});
-    mapstraction.addEventListener( 'drag', function() { alert("drag")});
-    mapstraction.addEventListener( 'endPan', function() { alert("endPan")});
+    mapstraction.changeZoom.addHandler(function(n, s, a) { alert("You changed the zoom level")});
+    mapstraction.endPan.addHandler(function(n, s, a) {  alert("You panned the map")});
 }
